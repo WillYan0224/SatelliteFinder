52520f9a4ac904422c7b16f49858a936fc75fec3
yan <yanchung1998@gmail.com>
2026-01-14 23:59:56 +0800

    Frontend & backend OKAY (with ISS API)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,9 @@ import {
   Viewer,
   Cartesian3,
   Color,
-  Entity,
-  ScreenSpaceEventHandler,
-  ScreenSpaceEventType,
+  SampledPositionProperty,
+  JulianDate,
+  LagrangePolynomialApproximation,
 } from "cesium";
 
 window.CESIUM_BASE_URL = "/cesium";
@@ -17,44 +17,67 @@ const viewer = new Viewer("app", {
   geocoder: false,
 });
 
+viewer.clock.shouldAnimate = true;
+viewer.clock.multiplier = 1;
+
+const API_BASE = "http://127.0.0.1:8000";
+
+const issPosition = new SampledPositionProperty();
+issPosition.setInterpolationOptions({
+  interpolationDegree: 2,
+  interpolationAlgorithm: LagrangePolynomialApproximation,
+});
+
+let issEntity = viewer.entities.add({
+  id: "iss",
+  name: "ISS",
+  position: issPosition,
+  point: {
+    pixelSize: 10,
+    color: Color.CYAN,
+    outlineColor: Color.WHITE,
+    outlineWidth: 2,
+  },
+  label: {
+    text: "ISS",
+    font: "16px sans-serif",
+    fillColor: Color.WHITE,
+    pixelOffset: new Cartesian3(0, -20, 0),
+  },
+  // Trail
+  path: {
+    leadTime: 0,
+    trailTime: 15 * 60, //sec * fequency -> n-mins
+    width: 2,
+  },
+});
+
+// Camera tracking
+viewer.trackedEntity = issEntity;
+
 async function fetchISS() {
   const res = await fetch("http://127.0.0.1:8000/api/iss");
   if (!res.ok) throw new Error("ISS API failed");
   return await res.json();
 }
 
-async function addOrUpdateISS() {
+async function addSampleISS() {
   const iss = await fetchISS();
+
   const lat = Number(iss.latitude);
   const lon = Number(iss.longitude);
   const altKm = Number(iss.altitude ?? 420);
 
   const pos = Cartesian3.fromDegrees(lon, lat, altKm * 1000);
 
-  let ent = viewer.entities.getById("iss");
-  if (!ent) {
-    ent = viewer.entities.add({
-      id: "iss",
-      name: "ISS",
-      position: pos,
-      point: {
-        pixelSize: 10,
-        color: Color.CYAN,
-        outlineColor: Color.WHITE,
-        outlineWidth: 2,
-      },
-      label: {
-        text: "ISS",
-        font: "16px sans-serif",
-        fillColor: Color.WHITE,
-        pixelOffset: new Cartesian3(0, -20, 0),
-      },
-    });
-    viewer.flyTo(ent);
-  } else {
-    ent.position = pos;
+  const now = viewer.clock.currentTime.clone();
+  issPosition.addSample(now, pos);
+
+  if (!addSampleISS._inited) {
+    await viewer.flyTo(issEntity);
+    addSampleISS._inited = true;
   }
 }
 
-addOrUpdateISS();
-setInterval(addOrUpdateISS, 5000);
+addSampleISS();
+setInterval(addSampleISS, 3000); // for better performance
